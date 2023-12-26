238de4679ea7e8e6341166de6412ea92cf837220
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2023-12-27 00:53:05 +0300

    UPD App.config "ADD Hotel Info"

--- Hotel/MainMenu/frmMainMenu.cs
+++ Hotel/MainMenu/frmMainMenu.cs
@@ -1,11 +1,6 @@
 ﻿using Guna.UI2.WinForms;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
